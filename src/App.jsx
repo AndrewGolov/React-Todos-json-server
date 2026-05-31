f874a100bf5dc9cab583eb773b3d27e05a13dd0c
AndrewGolov <telegram @andre_g_life>
2026-06-01 00:06:23 +0300

    refactoring code, delete duplicate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
 import { useState } from 'react';
 import { Loader, TodoListComponent, ActionBar } from './components';
-import { createTask, updateTask, searchTasks } from './utils';
+import { createTask, searchTasks } from './utils';
 import { AppContext } from './context/AppContext';
 import { useGetDataTodos } from './hooks';
+import { requestCompleteTask } from './requests/request-complete-task';
 
 export const App = () => {
-	const [searchResults, setSearchResults] = useState([]);
+	const [isSorted, setIsSorted] = useState(false);
+	const { dataTodos, isLoadingJsonServer, setterDataTodos } = useGetDataTodos(isSorted);
 	const [isAddingTask, setIsAddingTask] = useState(false);
 	const [isOpenSearch, setIsOpenSearch] = useState(false);
 	const [isSearchingMode, setIsSearchingMode] = useState(false);
-	const [isSorted, setIsSorted] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
-	const { dataTodos, isLoadingJsonServer, setterDataTodos } = useGetDataTodos(isSorted);
+	const [searchPhrase, setSearchPhrase] = useState('');
 
 	const onClickAddBtn = () => {
 		setIsAddingTask((prev) => !prev);
@@ -38,35 +39,9 @@ export const App = () => {
 	};
 
 	const handleCompletedTask = (idTask) => {
-		const task = dataTodos.find((task) => task.id === idTask);
+		const task = dataTodos.find((todo) => todo.id === idTask);
 		if (!task) return;
-		updateTask(idTask, {
-			completed: !task.completed,
-		})
-			.then(() => {
-				setterDataTodos((prev) =>
-					prev.map((task) =>
-						task.id === idTask
-							? {
-									...task,
-									completed: !task.completed,
-								}
-							: task,
-					),
-				);
-
-				setSearchResults((prev) =>
-					prev.map((task) =>
-						task.id === idTask
-							? {
-									...task,
-									completed: !task.completed,
-								}
-							: task,
-					),
-				);
-			})
-			.catch((error) => console.log('Ошибка запроса ...', error));
+		requestCompleteTask(idTask, task, setterDataTodos, setSearchPhrase);
 	};
 
 	const handleSortList = () => {
@@ -75,7 +50,7 @@ export const App = () => {
 
 	const clearSearch = () => {
 		setIsSearchingMode(false);
-		setSearchResults([]);
+		setSearchPhrase('');
 	};
 	const onClickSearchBtn = () => {
 		setIsOpenSearch((prev) => !prev);
@@ -87,20 +62,20 @@ export const App = () => {
 		if (!searchPhrase.trim()) return;
 		setIsSearchingMode(true);
 		searchTasks(searchPhrase)
-			.then((respData) => {
-				setSearchResults(respData);
+			.then(() => {
+				setSearchPhrase(searchPhrase);
 			})
 			.catch((error) => {
 				console.error('Ошибка при поиске задач', error);
 
-				setSearchResults([]);
+				setSearchPhrase('');
 			});
 	};
 
 	if (isLoadingJsonServer) {
 		return <Loader />;
 	}
-	const renderTodos = isSearchingMode ? searchResults : dataTodos;
+	const renderTodos = isSearchingMode ? dataTodos.filter((todo) => todo.title.includes(searchPhrase)) : dataTodos;
 
 	return (
 		<AppContext
